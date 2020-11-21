a9737c892b7b66fd5661e72a432b4191548b59b0
QTD <tranduc811@gmail.com>
2020-11-21 15:29:53 +0700

    feat: Quotes - Index, Show, Create

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,6 +1,26 @@
 class QuotesController < Rulers::Controller
+  def index
+    @quotes = FileModel.all.map(&:hash)
+    render :index
+  end
+
   def a_quote
     render :a_quote, noun: :winking
+  end
+
+  def quote_1
+    quote1 = Rulers::Model::FileModel.find(1)
+    render :quote, obj: quote1
+  end
+
+  def new_quote
+    attrs = {
+      'submitter' => 'web user',
+      'quote' => 'A picture is worth a thousand pixels',
+      'attribution' => 'Me'
+    }
+    m = FileModel.create attrs
+    render :quote, obj: m
   end
 
   def exception
